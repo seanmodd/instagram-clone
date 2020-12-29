304812e46b63af5672377ba2ed2f653df55d5e0a
Sean <sean@vestaridefair.com>
2020-12-28 17:11:24 -0800

    first commit

---
+++ ig-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e09c16a1ab0df6512c2285bc6de10200'),
+    },
+  },
+});
---
+++ ig-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fee93ae8-c47e-47d3-aca8-33d97170ee3e'
+};
